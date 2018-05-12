ebd8f7acb92b998a11744981b743896c4c52f48a
RekoLee <springworder@gmail.com>
2018-05-12 13:56:52 +0800

    login logout

--- Assets/Scripts/Core/User.cs
+++ Assets/Scripts/Core/User.cs
@@ -4,5 +4,28 @@ using UnityEngine;
 
 public class User
 {
+    //用户数据
+    public UserData userData;
 
+    /// <summary>
+    /// 用户连接对象
+    /// </summary>
+    public Conn conn;
+
+    public User(UserData data, Conn _conn)
+    {
+        this.userData = data;
+        this.conn = _conn;
+    }
+
+    /// <summary>
+    /// 供逻辑层调用的send
+    /// </summary>
+    /// <param name="proto"></param>
+    public void Send(BaseProtocol proto)
+    {
+        if (conn == null)
+            return;
+        Connector.instance.Send(conn, proto);
+    }
 }
--- Assets/Scripts/Logic/HandleConnMsg.cs
+++ Assets/Scripts/Logic/HandleConnMsg.cs
@@ -18,5 +18,51 @@ public class HandleConnMsg
 
     }
 
+    /// <summary>
+    /// 接受到服务器发过来的Login协议  判断能否登录
+    /// 服务器：Login|1/0
+    /// </summary>
+    /// <param name="conn"></param>
+    /// <param name="_protocol"></param>
+    public void MsgLogin(Conn conn,BaseProtocol _protocol)
+    {
+        BytesProtocol proto = _protocol as BytesProtocol;
+        int start = 0;
+        string protoName = proto.GetString(start,ref start);
+        int status = proto.GetInt(start,ref start).Value;
+        if(status==0)
+        {
+            //TODO:登录不成功
+        }
+        else if(status==1)
+        {
+            //TODO:登录成功
+        }
+    }
 
+
+    /// <summary>
+    /// 注册协议 判断注册是否成功
+    /// 服务器： Register|1/0
+    /// </summary>
+    /// <param name="conn"></param>
+    /// <param name="_protocol"></param>
+    public void MsgRegister(Conn conn,BaseProtocol _protocol)
+    {
+        BytesProtocol proto = _protocol as BytesProtocol;
+
+        int start = 0;
+        string protoName = proto.GetString(start,ref start);
+        int status = proto.GetInt(start,ref start).Value;
+
+        if(status==0)
+        {
+            //TODO:注册不成功
+        }
+        else if(status==1)
+        {
+            //TODO：注册成功
+        }
+
+    }
 }
